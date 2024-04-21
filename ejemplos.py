de0e08707ed119d46fe2bfb342ea638c33fb4e63
Carlos Urias <hicarlosurias@gmail.com>
2024-04-21 10:46:06 -0700

    🐛 Importar nueva funcion en ejemplos
    se importó la nueva funcion en el archivo ejemplos

--- ejemplos.py
+++ ejemplos.py
@@ -1,17 +1,17 @@
-from numero_a_texto import numero_a_texto
+from numero_a_texto import num_txt
 
 # Ejemplo 1
-print(numero_a_texto("123.45"))
+print(num_txt("123.45"))
 # Output: "ciento veintitrés pesos con cuarenta y cinco centavos"
 
 # Ejemplo 2
-print(numero_a_texto("9876.54"))
+print(num_txt("9876.54"))
 # Output: "nueve mil ochocientos setenta y seis pesos con cincuenta y cuatro centavos"
 
 # Ejemplo 3
-print(numero_a_texto("456.789"))
+print(num_txt("456.789"))
 # Output: "cuatrocientos cincuenta y seis pesos con setenta y ocho centavos"
 
 # Ejemplo 4
-print(numero_a_texto("9999999.99"))
+print(num_txt("9999999.99"))
 # Output: "nueve millones novecientos noventa y nueve mil novecientos noventa y nueve pesos con noventa y nueve centavos"
